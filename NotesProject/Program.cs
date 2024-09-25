36234005e1d12432607dee6be86f39221cdee333
MohamedHassan2004 <147160046+MohamedHassan2004@users.noreply.github.com>
2024-09-25 11:55:10 +0300

    Add project files.

---
+++ NotesProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
